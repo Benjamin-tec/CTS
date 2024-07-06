d4202700202c699e3c74e48e5661ca010e95d474
be.burgar <be.burgar@iskratel.si>
2024-07-06 14:42:09 +0200

    add unittest

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-@app.route('/ping', methods=['GET'])
-def ping():
-    return "200 OK", 200
+    @app.route('/ping', methods=['GET'])
+    def ping():
+        return 'pong', 200
 
-if __name__ == '__main__':
-    app.run()
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(host='0.0.0.0', port=8080)
+
